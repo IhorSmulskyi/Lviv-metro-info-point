52464084f9cbda90837a2ba6c19349e73ebcfd77
SmulskyiIhor <smulia755@gmail.com>
2019-04-27 12:38:37 +0300

    added initialize, calculate_price, calculate_time

--- metro_infopoint.rb
+++ metro_infopoint.rb
@@ -1,20 +1,40 @@
+require 'yaml'
+
 class MetroInfopoint
   def initialize(path_to_timing_file:, path_to_lines_file:)
-    # initialization here
+    @timing_data = YAML.load_file(path_to_timing_file)['timing']
+    puts @lines_data = YAML.load_file(path_to_lines_file)
   end
 
   def calculate(from_station:, to_station:)
-    { price: calculate_price(from_station: from_station, to_station: to_station),
-      time: calculate_time(from_station: from_station, to_station: to_station) }
+    { price: calculate_price(from_station: from_station, to_station: to_station) }
+    { time: calculate_time(from_station: from_station, to_station: to_station) }
   end
 
   def calculate_price(from_station:, to_station:)
-    # your implementation here
+    start = @timing_data.index(@timing_data.find { |e| e['start'].to_s == from_station })
+    finish = @timing_data.index(@timing_data.find { |e| e['start'].to_s == to_station })
+    array_of_station = []
+    if start > finish
+      @timing_data[start...finish].each { |i| array_of_station.push(i['price']) }
+    elsif start < finish
+      @timing_data[start...finish].reverse_each { |i| array_of_station.push(i['price']) }
+    end
+    puts array_of_station.sum
   end
 
   def calculate_time(from_station:, to_station:)
-    # your implementation here
+    start = @timing_data.index(@timing_data.find { |e| e['start'].to_s == from_station })
+    finish = @timing_data.index(@timing_data.find { |e| e['start'].to_s == to_station })
+    array_of_station = []
+    if start > finish
+      @timing_data[start...finish].each { |i| array_of_station.push(i['time']) }
+    elsif start < finish
+      @timing_data[start...finish].reverse_each { |i| array_of_station.push(i['time']) }
+    end
+    puts array_of_station.sum
   end
-
-  p "hello word"
 end
+
+info = MetroInfopoint.new(path_to_timing_file: './config/timing1.yml', path_to_lines_file: './config/config.yml')
+info.calculate(from_station: 'shevchenkivska', to_station: 'kiborgiv')
